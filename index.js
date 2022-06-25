db5754c367db803d8eee79788fc7d2aa459d18a8
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-06-25 18:34:44 -0400

    added questions showing during quiz time

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var timer = document.getElementById('timer')
 var start = document.getElementById('start')
+var questionTitle = document.getElementById('questionTitle')
+var a = document.getElementById('a')
+var b = document.getElementById('b')
+var c = document.getElementById('c')
+var d = document.getElementById('d')
 
 var time = 99
 
 start.addEventListener("click", function() {
     var timeStart = setInterval(() => {
         start.setAttribute("style", "display:none;")
+        timer.setAttribute("style", "display:flex;")
         time--;
         timer.textContent = time
        
@@ -14,8 +20,32 @@ start.addEventListener("click", function() {
                 console.log('work')
                 start.setAttribute("style", "display:flex;")
                 timer.setAttribute("style", "display:none;")
+                
             }
-        
+
+            if(time > 0) {
+                questionTitle.setAttribute('style', 'display:flex;')
+                a.setAttribute('style', 'display:flex;')
+                b.setAttribute('style', 'display:flex;')
+                c.setAttribute('style', 'display:flex;')
+                d.setAttribute('style', 'display:flex;')
+                console.log('hio')
+            } else {
+                questionTitle.setAttribute('style', 'display:none;')
+                a.setAttribute('style', 'display:none;')
+                b.setAttribute('style', 'display:none;')
+                c.setAttribute('style', 'display:none;')
+                d.setAttribute('style', 'display:none;')
+                console.log('hio')
+            }
+
+
+            if(time===0) {
+                time = 99
+            }
     }, 100);
-    
 })
+
+
+
+
