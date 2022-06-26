6f18c5187be36dbbcd1b18685d41259b0061e307
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-06-26 15:06:08 -0400

    finished quiz functionality and added some styling

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var checkD = document.getElementById('checkD')
 
 var questionNumber = 1
 
+
 var time = 99
 
 start.addEventListener("click", function() {
@@ -22,6 +23,7 @@ start.addEventListener("click", function() {
         timer.setAttribute("style", "display:flex;")
         time--;
         timer.textContent = time
+        questionChecker()
        
 
             if(time > 0) {
@@ -42,6 +44,7 @@ start.addEventListener("click", function() {
                 start.setAttribute("style", "display:flex;")
                 timer.setAttribute("style", "display:none;")
                 time = 99
+                questionNumber = 1
             }
 
         
@@ -51,9 +54,11 @@ start.addEventListener("click", function() {
 })
 
 
+
 checkA.addEventListener("click", function(){
         if(questionNumber === 1) {
-            questionNumber = questionNumber + 1
+            questionTitle.textContent = ''
+        questionNumber = questionNumber + 1
             console.log(questionNumber)
 
         }
@@ -63,12 +68,14 @@ checkB.addEventListener("click", function(){
         if(questionNumber === 2) {
             questionNumber = questionNumber + 1
             console.log(questionNumber)
+            questionChecker()
 
         }
 
         if(questionNumber === 3) {
             questionNumber = questionNumber + 1
             console.log(questionNumber)
+            questionChecker()
 
         }
     })
@@ -78,6 +85,7 @@ checkC.addEventListener("click", function(){
         if(questionNumber === 5) {
             questionNumber = questionNumber + 1
             console.log(questionNumber)
+            questionChecker()
 
         }
     })
@@ -87,12 +95,55 @@ checkD.addEventListener("click", function(){
         if(questionNumber === 4) {
             questionNumber = questionNumber + 1
             console.log(questionNumber)
+            questionChecker()
 
         }
     })
 
-
+function questionChecker() {
+    if(questionNumber === 1) {
+        questionTitle.textContent = 'What does "document.getElementbyId" do in javascript?'
+        checkA.textContent = 'Targets an ID in your HTML document'
+        checkB.textContent = 'Selects an HTML class'
+        checkC.textContent = 'acts as an event listener'
+        checkD.textContent = 'creates a new HTML file'
     
+    }
+    
+    if(questionNumber === 2) {
+        questionTitle.textContent = 'What does "addEventListener" do in javascript?'
+        checkA.textContent = 'Targets a class in your HTML document'
+        checkB.textContent = 'adds a function that starts based on an event that occurs'
+        checkC.textContent = 'Styles your h1'
+        checkD.textContent = 'opens a new browser'
+        console.log('hi')
+    }
+    
+    if(questionNumber === 3) {
+        questionTitle.textContent = 'What does ".innerHTML" do?'
+        checkA.textContent = 'selects a class'
+        checkB.textContent = 'dynamically inputs html code from javascript'
+        checkC.textContent = 'styles your html'
+        checkD.textContent = 'prevents defaulting' 
+    }
+    
+    if(questionNumber === 4) {
+        questionTitle.textContent = 'What does preventing default do in javascript?'
+        checkA.textContent = 'Selects an id'
+        checkB.textContent = 'Stops us from losing our house'
+        checkC.textContent = 'Styles our HTML'
+        checkD.textContent = 'Stops us from refreshing the page when an event occurs' 
+    }
+    
+    if(questionNumber === 5) {
+        questionTitle.textContent = 'Why do we add HTML dynamically though javascript'
+        checkA.textContent = 'We dont'
+        checkB.textContent = 'It looks cooler'
+        checkC.textContent = 'To allow for a more interactive application'
+        checkD.textContent = 'All of the above' 
+    }
+
+} 
 
 
 
