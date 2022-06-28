8a6c75c627a4cb01304a812804561c522710a7d6
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-06-27 20:15:31 -0400

    added scoring system and congratulations page,getting bugs with leaderboard and storage

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var b = document.getElementById('b')
 var c = document.getElementById('c')
 var d = document.getElementById('d')
 var wrong = document.getElementById('results-text')
+var submitScore = document.getElementById('submit-score')
+var initialsOfUser = document.getElementById('initials-value')
+var leaderboardButton = document.getElementById('leaderboard-button')
+var scoreGetter = document.getElementById('scoreGetter')
+var displayLeaderboard = document.getElementById('leaderboard')
+var leaderboardData = document.getElementById('leaderboard-data')
 
 var checkA = document.getElementById('checkA')
 var checkB = document.getElementById('checkB')
@@ -17,7 +23,31 @@ var questionNumber = 1
 var playerScore = 0
 
 
+
+var leaderboard = []
+
 var time = 99
+init()
+function init() {
+    JSON.parse(localStorage.getItem("leaderboard"));
+}
+
+function storeData(e) {
+    e.preventDefault();
+
+    
+    var player = {
+        intitials: initialsOfUser.value,
+        score: playerScore
+    }
+
+    leaderboard.push(player)
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard))
+    
+    displayLeaderboard.setAttribute("style", "displaty:flex")
+    leaderboardData.textContent = storedInitials
+
+}
 
 start.addEventListener("click", function() {
     var timeStart = setInterval(() => {
@@ -26,15 +56,29 @@ start.addEventListener("click", function() {
         time--;
         timer.textContent = time
         questionChecker()
-       
 
-            if(time > 0) {
+
+
+            if(time > 0 && questionNumber !== 6) {
                 questionTitle.setAttribute('style', 'display:flex;')
                 a.setAttribute('style', 'display:flex;')
                 b.setAttribute('style', 'display:flex;')
                 c.setAttribute('style', 'display:flex;')
                 d.setAttribute('style', 'display:flex;')
                 
+            } else if (questionNumber === 6) {
+                questionTitle.setAttribute('style', 'display:none;')
+                a.setAttribute('style', 'display:none;')
+                b.setAttribute('style', 'display:none;')
+                c.setAttribute('style', 'display:none;')
+                d.setAttribute('style', 'display:none;')
+                timer.setAttribute("style", "display:none;")
+                scoreGetter.textContent = "Your score was: " + playerScore
+                time = 99
+                questionNumber = 1
+                clearInterval(timeStart)
+                console.log(time)
+
             } else {
                 questionTitle.setAttribute('style', 'display:none;')
                 a.setAttribute('style', 'display:none;')
@@ -47,6 +91,7 @@ start.addEventListener("click", function() {
                 timer.setAttribute("style", "display:none;")
                 time = 99
                 questionNumber = 1
+                playerScore = 0
             }
 
         
@@ -59,9 +104,15 @@ start.addEventListener("click", function() {
 
 checkA.addEventListener("click", function(){
         if(questionNumber === 1) {
-            questionTitle.textContent = ''
+
         questionNumber = questionNumber + 1
             console.log(questionNumber)
+            playerScore += 3
+
+        }  else {
+            wrong.setAttribute('style', 'display: flex;')
+            wrong.textContent = 'Wrong!'
+            playerScore -= 1
 
         }
     })
@@ -71,14 +122,17 @@ checkB.addEventListener("click", function(){
             questionNumber = questionNumber + 1
             console.log(questionNumber)
             questionChecker()
+            playerScore += 3
 
         }  else if(questionNumber === 3) {
             questionNumber = questionNumber + 1
             console.log(questionNumber)
             questionChecker()
+            playerScore += 3
         } else {
             wrong.setAttribute('style', 'display: flex;')
             wrong.textContent = 'Wrong!'
+            playerScore -= 1
 
         }
 
@@ -91,6 +145,13 @@ checkC.addEventListener("click", function(){
             questionNumber = questionNumber + 1
             console.log(questionNumber)
             questionChecker()
+            playerScore += 3
+
+        }  else {
+            wrong.setAttribute('style', 'display: flex;')
+            wrong.textContent = 'Wrong!'
+            playerScore -= 1
+            
 
         }
     })
@@ -101,6 +162,12 @@ checkD.addEventListener("click", function(){
             questionNumber = questionNumber + 1
             console.log(questionNumber)
             questionChecker()
+            playerScore += 3
+
+        }  else {
+            wrong.setAttribute('style', 'display: flex;')
+            wrong.textContent = 'Wrong!'
+            playerScore -= 1
 
         }
     })
@@ -158,7 +225,10 @@ function questionChecker(event) {
 
     }
 
-} 
+    if(questionNumber === 6) {
+        submitScore.setAttribute("style", "display:flex;") 
+    }
 
+}
 
-
+leaderboardButton.addEventListener("click", storeData)
